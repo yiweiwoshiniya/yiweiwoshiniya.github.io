47ca199325dfaa723b694a2cf3fbf2fb1bac54a9
yiweiwoshiniya <yiweiwoshiniya@hotmail.co.uk>
2016-12-28 16:13:24 +0800

    biaobai

---
+++ biaobai/bdtj.js
@@ -0,0 +1,9 @@
+//baidutongji-love
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?95cceda5c630eda8a1cc8e000b3a93e0";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
